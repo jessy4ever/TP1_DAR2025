0349cbe8189241f9058682026f83d9fd3b01a2d4
Yasmine Abbes <yasminabbes473@gmail.com>
2025-10-18 01:45:30 +0200

    Ajout de l'activité 1-2 : service de calculatrice TCP

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -1,21 +1,24 @@
 package clientPackage;
 
-import java.io.IOException;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        try {
-            System.out.println("Je suis un client pas encore connecté...");
-            
-            Socket socket = new Socket("192.168.56.1", 5000);
-            System.out.println("Je suis un client connecté !");
-            
-            // Fermeture de la socket
-            socket.close();
-            System.out.println("Connexion fermée.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		try (Socket socket = new Socket("localhost", 5000);
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			Scanner sc = new Scanner(System.in)) {
+			System.out.println("Client connecté. Entrez un entier : ");
+			int x = sc.nextInt();
+			// Envoi de l'entier au serveur
+			out.writeInt(x);
+			// Réception du résultat
+			int resultat = in.readInt();
+			System.out.println("Résultat reçu du serveur : " + resultat);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
