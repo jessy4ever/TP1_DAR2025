395f226d58840581aadd179bf39e07c787363b1a
Yasmine Abbes <yasminabbes473@gmail.com>
2025-10-18 23:54:38 +0200

    Activité 1-1 : Première connexion Client/Serveur

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -1,44 +1,20 @@
 package clientPackage;
 
-import java.io.*;
-import java.net.*;
-import java.util.Scanner;
-import objectPackage.Operation;
+import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 5000)) {
-            System.out.println("Connecté au serveur !");
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            Scanner sc = new Scanner(System.in);
-
-            boolean continuer = true;
-            while (continuer) {
-                System.out.print("Entrez le premier nombre : ");
-                double op1 = sc.nextDouble();
-
-                System.out.print("Entrez l’opérateur (+, -, *, /) : ");
-                char operateur = sc.next().charAt(0);
-
-                System.out.print("Entrez le deuxième nombre : ");
-                double op2 = sc.nextDouble();
-
-                Operation operation = new Operation(op1, operateur, op2);
-                out.writeObject(operation);
-                out.flush();
-
-                double resultat = in.readDouble();
-                System.out.println("Résultat reçu : " + resultat);
-
-                System.out.print("Voulez-vous continuer ? (1 = oui / 0 = non) : ");
-                int rep = sc.nextInt();
-                if (rep == 0) continuer = false;
-            }
-
-            sc.close();
-            System.out.println("Client terminé.");
-        } catch (Exception e) {
+        try {
+            System.out.println("Je suis un client pas encore connecté...");
+            
+            Socket socket = new Socket("localhost", 5000);
+            System.out.println("Je suis un client connecté !");
+            
+            // Fermeture de la socket
+            socket.close();
+            System.out.println("Connexion fermée.");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -1,45 +1,23 @@
 package serverPackage;
 
-import java.io.*;
-import java.net.*;
-import objectPackage.Operation;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class Server{
+public class Server {
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(5000)) {
-            System.out.println("Serveur en attente de connexion...");
-
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connecté !");
-
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-
-            while (true) {
-                Operation op = (Operation) in.readObject();
-
-                double resultat = 0;
-                switch (op.getOperateur()) {
-                    case '+': resultat = op.getOperande1() + op.getOperande2(); break;
-                    case '-': resultat = op.getOperande1() - op.getOperande2(); break;
-                    case '*': resultat = op.getOperande1() * op.getOperande2(); break;
-                    case '/': 
-                        if (op.getOperande2() != 0)
-                            resultat = op.getOperande1() / op.getOperande2();
-                        else
-                            System.out.println("Division par zéro !");
-                        break;
-                    default:
-                        System.out.println("Opérateur non reconnu !");
-                        break;
-                }
-
-                out.writeDouble(resultat);
-                out.flush();
-                System.out.println("Opération reçue : " + op.getOperande1() + " " + op.getOperateur() + " " + op.getOperande2() + " = " + resultat);
-            }
-
-        } catch (Exception e) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Je suis un serveur en attente de la connexion d'un client...");
+            
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Un client est connecté !");
+            
+            // Fermeture des sockets
+            clientSocket.close();
+            serverSocket.close();
+            System.out.println("Connexion fermée.");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
